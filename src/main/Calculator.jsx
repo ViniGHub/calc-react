1d528b513da1d88c4b487e99639c04c7cf522b25
Vinicius Rodrigues da Silva <vinicius.rodrigues2004@gmail.com>
2025-03-06 22:29:03 -0300

    Calculadora funcionando

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -4,15 +4,15 @@ import Button from "../components/Buttons"
 import Display from "../components/Display"
 
 const initialState = {
-    displayValue: "1000",
-    clearDislay: false,
+    displayValue: "0",
+    clearDisplay: false,
     operation: null,
     values: [0, 0],
     current: 0
 }
 
 export default class Calculator extends Component {
-    state = {...initialState}
+    state = { ...initialState }
 
     constructor() {
         super()
@@ -22,16 +22,61 @@ export default class Calculator extends Component {
         this.addDigit.bind(this)
     }
 
-    clearMemory() {
-        this.state = {...initialState}
+    clearMemory = () => {
+        this.setState({ ...initialState })
     }
 
-    setOperation(operation) {
+    setOperation = (operation) => {
+        if (this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true })
+        } else {
+            const equals = operation === "="
+            const currentOperation = this.state.operation
 
+            const values = [...this.state.values]
+
+            debugger;
+            try {
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+                if (isNaN(values[0]) || !isFinite(values[0])) {
+                    this.clearMemory();
+                    return
+                }
+            } catch(e) {
+                values[0] = this.state.values[0]
+            }
+            values[1] = 0
+
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
+        }
     }
 
-    addDigit(n) {
-        if (n === "." && this.state.displayValue)
+    addDigit = (n) => {
+        if (n === "." && this.state.displayValue.includes(".")) {
+            return
+        }
+
+        const clearDisplay = this.state.displayValue === "0" || this.state.clearDisplay
+        const currenValue = clearDisplay ? "" : this.state.displayValue
+        const displayValue = currenValue + n
+
+        this.setState({ displayValue, clearDisplay: false })
+
+        if (n !== ".") {
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[i] = newValue
+            this.setState({values})
+        }
+
+        
     }
 
     render() {
